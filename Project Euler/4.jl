2a03d12bd1b57971cf723655e7d287d7d10f3067
ANIL80531 <anil80531@gmail.com>
2025-04-19 12:12:05 +0000

    moving files

--- Project Euler/4.jl
+++ Project Euler/4.jl
@@ -6,16 +6,9 @@ of two 2-digit numbers is
 Find the largest palindrome made from the product
 of two 3-digit numbers.
 =#
-global x = 999
+global x = 999 # Largest three digit number
 global y = 999
 
-while x>0+
-    if string(x*y)[1:3]==string(x*y)[3:6]
-        println("Palindrome: ",x,"*",y," = ",x*y)
-    elseif length(x*y)!=6
-        continue
-    else
-        global x -= 1
-    end
+while x>900 && y>900
 
 end
